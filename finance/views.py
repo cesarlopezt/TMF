9cb5c00280e72d16a65aded8ace9a996b49618ae
calt08 <cesarlopez2808@gmail.com>
2019-10-19 05:06:04 -0400

    menu, functional mainpage, login, logout, create movements

--- finance/views.py
+++ finance/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from .models import Movement
+from django.views.generic import ListView, DetailView, CreateView
 
 
-def home(request):
-   # context = {
-   #    'Movements' : Movement.objects.all()
-   # }
-   return render(request, 'finance/userMainPage.html',{'title': 'Main Page'})
+# def home(request):
+#    context = {
+#       'title': 'Main Page',
+#       'Movements' : Movement.objects.all()
+#    }
+#    return render(request, 'finance/MainPage.html',context)
 
-def about(request):
-    return render(request, 'finance/base.html')
+class MovementListView(ListView):
+   model = Movement
+   template_name = "finance/MainPage.html"
+   context_object_name = 'Movements'
+   ordering = ['-datePosted']
+
+# class MovementDetailView(DetailView):
+#    model = Movement
+class MovementCreateView(CreateView):
+   model = Movement
+   fields = ['description', 'Amount']
+
+   def form_valid(self, form):
+      form.instance.user = self.request.user
+      return super().form_valid(form)
