c52187f7c8ce63e5b542c66cad842f1d6540b151
calt08 <cesarlopez2808@gmail.com>
2019-10-28 00:11:36 -0400

    filter for movements added

--- finance/views.py
+++ finance/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django import forms
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from .filters import MovementFilter
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
 from .models import Movement, Category, Type
 
 #-------------------------------------------------------------------------------
@@ -13,6 +14,11 @@ class MovementListView(ListView):
     template_name = "finance/MainPage.html"
     context_object_name = 'Movements'
     ordering = ['-date']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = MovementFilter(self.request.GET, self.get_queryset())
+        return context
 
     def get_queryset(self):
         '''Specifies that i only want the user that is currently logged in data.'''
