092ac006d803ca1c1a42e3804bedc4cd652a286e
calt08 <cesarlopez2808@gmail.com>
2019-11-04 22:13:31 -0400

    cleaner code adding static files for charts

--- finance/static/finance/script_Chart.js
+++ finance/static/finance/script_Chart.js
@@ -1,5 +1,4 @@
-console.log('Hello');
-
+// Chart Income
 var ctx = document.getElementById('myChartIncome');
 console.log(jsonDataI)
 var myChart = new Chart(ctx, {
@@ -10,39 +9,25 @@ var myChart = new Chart(ctx, {
             label: '',
             data: jsonDataI.default,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            borderWidth: 0
         }]
     },
     options: {
         title: {
             display: true,
             text: 'Income'
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
         }
     }
 });
 
+// Chart Expense
 var ctx = document.getElementById('myChartExpense');
 console.log(jsonDataI)
 var myChart = new Chart(ctx, {
@@ -53,35 +38,20 @@ var myChart = new Chart(ctx, {
             label: '',
             data: jsonDataE.default,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            borderWidth: 0
         }]
     },
     options: {
         title: {
             display: true,
             text: 'Expense'
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
         }
     }
 });
